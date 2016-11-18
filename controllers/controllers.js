3467327a81cc425ef7361cc9de844253c63b4653
tomtom28 <tomthompson828@gmail.com>
2016-11-17 22:41:53 -0500

    added routing structure and schema commands

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -11,19 +11,155 @@ var models = require('../models'); // Pulls out the Burger Models
 // sequelizeConnection.sync();
 
 
-// Create routes
+// GET Routes to render pages
 // ----------------------------------------------------
 
 // Index Redirect
-router.get('/', function (req, res) {
+router.get('/', function (req, res){
   res.redirect('/index');
 });
 
 
+// Index Page (DOM Render)
+router.get('/index', function (req, res){
 
-// Index Page 
-router.get('/index', function (req, res) {
-  // Render something...
+  // Render hompage (no handlebars)
+  res.render('index');
+
+});
+
+
+// User Sees All Countries in Database (DOM Render)
+router.get('/view/countries', function (req, res){
+
+  // Query Database for all Countries
+  models.Countries.findAll({}).then(function(data){
+
+    // Pass the returned data into a Handlebars object
+    var hbsObject = { countries: data };
+
+    // Render *addPlaces* template with *countries*
+    res.render('addPlaces', hbsObject);
+
+  });
+
+});
+
+
+// User Sees All States in Database (DOM Render)
+router.get('/view/states', function (req, res){
+
+  // Query Database for all States
+  models.States.findAll({}).then(function(data){
+
+    // Pass the returned data into a Handlebars object
+    var hbsObject = { states: data };
+
+    // Render *addPlaces* template with *states*
+    res.render('addPlaces', hbsObject);
+    
+  });
+
+});
+
+
+// User Sees All Cities in Database (DOM Render)
+router.get('/view/cities', function (req, res){
+
+  // Query Database for all States
+  models.Cities.findAll({}).then(function(data){
+
+    // Pass the returned data into a Handlebars object
+    var hbsObject = { cities: data };
+
+    // Render *addPlaces* template with *cities*
+    res.render('addPlaces', hbsObject);
+    
+  });
+
+});
+
+
+// User Sees All Bucket List entries in the Database (DOM Render)
+router.get('/view/bucketlist/:userId', function(req, res){
+
+  // Query Database for all Countries, States, Cities belonging to the user
+  models.Users.findAll({
+    where: {
+      id: userId
+    },
+    include: [models.Countries, models.States, models.Cities]
+  }).then(function(data){
+
+    // Pass the returned data into a Handlebars object
+    var hbsObject = { bucketlist: data };
+
+    // Render *addPlaces* template with *states*
+    res.render('viewAccount', hbsObject);
+    
+  });
+
+});
+
+// ----------------------------------------------------
+
+
+
+
+// POST Routes for Database changes
+// ----------------------------------------------------
+
+// Create a new User
+router.post('/create/user/:name/:facebook', function (req, res){
+
+  // Insert a new user to Users Table
+
+  // Redirect to index page
+  res.redirect('/index');
+
+});
+
+
+
+// Add/Remove a Country from Bucket List
+router.post('/:action/country/:userId/:countryId', function(req, res){
+
+  // (add) Relate Country Id to User
+    // Redirect to country page
+    res.redirect('/view/countries');
+
+  // (remove) Remove Country Id from User
+    // Redirect to bucket list page
+    res.redirect('/view/bucketlist');
+
+});
+
+
+// Add/Remove a State from Bucket List
+router.post('/:action/state/:userId/:stateId', function(req, res){
+
+  // (add) Relate State Id to User
+    // Redirect to state page
+    res.redirect('/view/states');
+
+  // (remove) Remove State Id from User
+    // Redirect to bucket list page
+    res.redirect('/view/bucketlist');
+
+});
+
+
+// Add/Remove a City from Bucket List
+router.post('/:action/city/:userId/:cityId', function(req, res){
+
+  // (add) Relate City Id to User
+    // Redirect to city page
+    res.redirect('/view/cities');
+
+  // (remove) Remove City Id from User
+    // Redirect to bucket list page
+    res.redirect('/view/bucketlist');
+
 });
 
 
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,44 @@
+-- NOTE: If you followed the readme steps, you can disregard this entire file.-
+
 -- Raw SQL for Database Creation (run this in MySQL Workbench)
 CREATE DATABASE bucketBuddiezDB;
 
--- Info on MySQL CLI commands to be added later...
+-- Then run the following in your Sequelize CLI to create the migrations and seed the database
+sequelize db:migrate && sequelize db:seed:all
+
+
+
+
+
+
+-- ==========================================================================================
+
+
+
+
+-- The following lines are for documentation, no need to copy for run anything after this...
+
+  -- Users Table Migration
+  ` sequelize model:create --name Users --attributes 'firstName:string lastName:string facebookId:integer' `
+
+
+
+  -- Countries Table Migration
+  ` sequelize model:create --name Countries --attributes 'countryName:string' `
+
+  -- States Table Migration
+  ` sequelize model:create --name States --attributes 'stateName:string' `
+
+  -- Cities Table Migration
+  ` sequelize model:create --name Cities --attributes 'cityName:string' `
+
+
+
+  -- Country Likes (all the countries that all the users like)
+  ` sequelize model:create --name CountryLikes --attributes 'userId:integer countryId:integer' `
+
+  -- State Likes (all the states that all the users like)
+  ` sequelize model:create --name StateLikes --attributes 'userId:integer stateId:integer' `
+
+  -- City Likes (all the cities that all the users like)
+  ` sequelize model:create --name CountryLikes --attributes 'userId:integer cityId:integer' `
