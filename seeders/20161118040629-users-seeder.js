773dfe834034c4ddb5a20eb2b67d9be83440c59c
tomtom28 <tomthompson828@gmail.com>
2016-11-18 01:21:45 -0500

    Added state, country, city seeders

--- seeders/20161118040629-users-seeder.js
+++ seeders/20161118040629-users-seeder.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    // 100000544140061 || 100001259538717
+
     // Add seeded Users to database (note that the date needs to be manually added here)
     return queryInterface.bulkInsert('Users', [
       {firstName: "Tommy", lastName: "Thompson", facebookId: 100000544140061, createdAt: new Date(), updatedAt: new Date()},
